e0a497c231c262194d3f629cbf8d64ff0eec7a4a
Realbaji16 <bajiemeks16@gmail.com>
2024-12-07 21:47:28 +0100

    new

--- src/components/header/Hadar.jsx
+++ src/components/header/Hadar.jsx
@@ -1,43 +1,38 @@
-import React from 'react'
-import Typed from 'react-typed'
-import './hadar.css'
-import CTA from './CTA' 
-import './header.css'
-import HeaderSocials from './HeaderSocials'
+import React from 'react';
+import { ReactTyped } from 'react-typed';
+import './hadar.css';
+import CTA from './CTA';
+import './header.css';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
 
-    const arr =[
+    const arr = [
         "Hi, I'm OBAJI CHUKWUEMEKA. ",
         "A front end developer.",
-         "Web Developer."
-    ]
+        "Web Developer."
+    ];
 
     return (
         <>
-        <div className='header-wrapper'>
-            <div className='main-info'>
-           
-                <Typed 
-                    className='typed-text'
-                    strings={arr}
-                    typeSpeed={40}
-                    backSpeed={60}
-                    loop
-                />
-                 <CTA />
+            <div className='header-wrapper'>
+                <div className='main-info'>
+                    <ReactTyped
+                        className='typed-text'
+                        strings={arr}
+                        typeSpeed={40}
+                        backSpeed={60}
+                        loop
+                    />
+                    <CTA />
+                </div>
+            </div>    
+            <div className='container header__container'>
+                <HeaderSocials />
+                <a href='#contact' className='scroll__down'>Scroll Down</a>
             </div>
-        </div>    
-        <div className='container header__container'>
-           
-           <HeaderSocials />
-           <a href='#contact' className='scroll__down'>Scroll Down</a>
-           </div>
+        </>
+    );
+};
 
-      
-       </>
-    
-    )
-}
-
-export default Header
+export default Header;
